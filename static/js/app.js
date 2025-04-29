16ec9cff97f12493a735d68e03ebb24fcbf95e31
g2charlotte <charlottegilchrist3@gmail.com>
2025-04-29 13:46:53 -0500

    banterbird with login page

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,9 @@
-const username = "admin";
-function renderPost(post) {
+let username = localStorage.getItem('username');
+if (!username){
+  window.location.href = "/login";
+}
+
+  function renderPost(post) {
     const template = document
       .getElementById("post-template")
       .content.cloneNode(true);
@@ -51,10 +55,11 @@ function renderPost(post) {
   }
   window.onload = async () => {
     try {
+      document.getElementById("username").innerText = username;
       const response = await fetch("/api/posts");
       const posts = await response.json();
       posts.forEach((post) => renderPost(post));
     } catch (error) {
       console.error("Error fetching posts:", error);
-    }
+    }   
   };
